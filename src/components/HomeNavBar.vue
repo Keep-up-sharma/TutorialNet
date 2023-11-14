73ca3970411ac08fb56a6fbddf73c0a2c6060670
keep-up-sharma <keepupsharma@gmail.com>
2023-11-14 16:22:05 -0600

    Navbar changes
    Made navigation bar dark
    Adjusted styling

--- src/components/HomeNavBar.vue
+++ src/components/HomeNavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <CreateProjectForm />
     <LoginForm @login="$emit('login')" />
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-dark navbar-expand-lg navbar-light bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">&nbsp;&nbsp;&nbsp;TutorialNet</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -13,21 +13,17 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#" @click="navigateToHome">Home</a>
                     </li>
-                    <li class="nav-item dropdown Hi" v-if="loggedIn">
+                    <li class="nav-item dropdown" v-if="loggedIn">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             Hi <div id="username">{{ username }}</div>
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#projectModal" href="#">Add
-                                    Tutorial</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
                             <li><a class="dropdown-item" @click="logout" href="#">Logout</a></li>
                         </ul>
                     </li>
+                    <li class="nav-item"><a class="nav-link" data-bs-toggle="modal" data-bs-target="#projectModal" href="#">Add
+                        Tutorial</a></li>
                     <li v-if="!loggedIn" class="nav-item">
                         <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
                     </li>
@@ -78,7 +74,10 @@ export default {
 </script>
 <style>
 .navbar {
+    position: absolute;
+    min-height: 80px;
     min-width: 100vw;
+    z-index: 5;
 }
 
 .Hi.nav-item {
